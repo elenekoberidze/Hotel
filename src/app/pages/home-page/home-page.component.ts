c2a6feaedd29a1b93494a0962508d9ef0f6a6fb8
koberidzeelene <elene.koberidze@icloud.com>
2025-04-05 19:52:41 +0400

    .

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -4,6 +4,7 @@ import {  RouterLink } from '@angular/router';
 import { Rooms } from '../../modules/rooms.model';
 import { HotelBookingService } from '../../service/hotel-booking.service';
 import { CommonModule } from '@angular/common';
+import { HotelsService } from '../../service/hotels.service';
 
 
 @Component({
@@ -60,7 +61,46 @@ export class HomePageComponent implements OnInit {
       }
     ]}
   ];
+  this.currentHotel = this.hotels[this.currentIndex];
+  this.changeBackgroundImage();
   }
-constructor( private hotelBookingService: HotelBookingService) {}
+constructor( private hotelBookingService: HotelBookingService, private hotelsService: HotelsService) {}
+hotels: any[] = [
+  {
+    id: 1,
+    name: "The Biltmore Hotel Tbilisi",
+    address: "29 Rustaveli Ave, Tbilisi, Tbilisi, 0108",
+    city: "Tbilisi",
+    featuredImage: "https://images.trvl-media.com/lodging/16000000/15840000/15835100/15835033/41cbdcb1.jpg?impolicy=resizecrop&rw=1200&ra=fit",
+    rooms: []
+  },
+  {
+    id: 2,
+    name: "Courtyard by Marriott Tbilisi",
+    address: "4, Freedom Square, Tbilisi, 0105",
+    city: "Tbilisi",
+    featuredImage: "https://images.trvl-media.com/lodging/1000000/920000/916400/916376/3e65a896.jpg?impolicy=resizecrop&rw=1200&ra=fit",
+    rooms: []
+  },
+  {
+    id: 3,
+    name: "Radisson Blu Iveria Hotel Tbilisi",
+    address: "1 Republic square, Tbilisi, 0108",
+    city: "Tbilisi",
+    featuredImage: "https://images.trvl-media.com/lodging/3000000/2620000/2614700/2614694/84d23097.jpg?impolicy=resizecrop&rw=1200&ra=fit",
+    rooms: []
+  }
+];
 
+currentHotel: any;
+currentIndex = 0;
+
+changeBackgroundImage(): void {
+  setInterval(() => {
+    this.currentIndex = (this.currentIndex + 1) % this.hotels.length;
+    this.currentHotel = this.hotels[this.currentIndex];
+  }, 3000); 
 }
+}
+
+
